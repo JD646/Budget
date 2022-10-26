66dae418310d6d9f1e392302fd60393604691148
Jonas <jonas@Jonass-MacBook-Pro.local>
2022-10-26 01:31:16 -0400

    squashed bug, implemented folder reading, ensured csv files only

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+import os
 import pandas as pd
 import pprint
 import sys
 from config import Configuration
-from scotia import scotia_visa
-from scotia import scotia_debit
-
-file_path = sys.argv[1]
+from scotia import scotia_visa, scotia_debit
 
 
-# #Creating a dataframe out of the CSV file
-# df = pd.read_csv(file_path,
-#                     names=['Date','Name','Transaction'])
+os.chdir('bank')
+x = os.listdir()
+csvs = []
 
-# #Creation of lists
-# date = df['Date'].values.tolist()
-# name = df['Name'].values.tolist()
-# transaction = df['Transaction'].values.tolist()
+for file in x:
+    if '.csv' in file:
+        csvs.append(file)
+        
 
-visa = scotia_visa(visa_path)
-visa.clean_up()
+for file in csvs:
+    if 'Scotia_Visa' in file:
+        visa_path = file
+        visa = scotia_visa(visa_path)
+        visa.clean_up()
 
-debit = scotia_debit(debit_path)
-debit.clean_up()
-#s_debit = scotia_debit(date, transaction, name)
 
+    elif "Scotia_Debit" in file:
+        debit_path = file
+        debit = scotia_debit(debit_path)
+        debit.clean_up()
+
+#testcases
 pprint.pprint(visa.fixed_date)
+pprint.pprint(visa.fixed_name)
+pprint.pprint(debit.fixed_date)
+pprint.pprint(debit.fixed_name)
