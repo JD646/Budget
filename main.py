ba2dc81496ba67ffbc4b883c4c55c247f2624cf4
Jonas Diec <jd.code.416@gmail.com>
2022-12-15 17:50:10 -0800

    Version for MBP where files are not transferred to D drive

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ for file in csvs:
                      'Amount': visa.fixed_transaction}
         visa_df = pd.DataFrame(visa_dict)
         visa_df.to_csv('visa_processed.csv', index = False)
-        shutil.move("/home/anubis/git/budget/bank/visa_processed.csv","/mnt/d/banking_files/visa_processed.csv")
 
     elif file.startswith('Scotia_Debit'):
         debit_path = file
@@ -45,8 +44,7 @@ for file in csvs:
                      'Amount': debit.fixed_transaction}
         debit_df = pd.DataFrame(debit_dict)
         debit_df.to_csv('debit_processed.csv', index = False)
-        shutil.move("/home/anubis/git/budget/bank/debit_processed.csv","/mnt/d/banking_files/debit_processed.csv")
-    
+        
     elif file.startswith('transactions'):
         mint_path = file
         mint = mint(mint_path)
@@ -59,8 +57,7 @@ for file in csvs:
                      'Amount': mint.fixed_transaction}
         mint_df = pd.DataFrame(mint_dict)
         mint_df.to_csv('mint_processed.csv', index = False)
-        shutil.move("/home/anubis/git/budget/bank/mint_processed.csv","/mnt/d/banking_files/mint_processed.csv")
-    
+        
     elif file.startswith('td'):
         td_path = file
         td = td(td_path)
@@ -73,7 +70,6 @@ for file in csvs:
                      'Amount': td.fixed_transaction}
         td_df = pd.DataFrame(td_dict)
         td_df.to_csv('td_processed.csv', index = False)
-        shutil.move("/home/anubis/git/budget/bank/td_processed.csv","/mnt/d/banking_files/td_processed.csv")
-
+        
 
 print("Everything checks out chief, Files processed")
